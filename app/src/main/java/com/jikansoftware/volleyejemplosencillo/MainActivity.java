38fcf75fd8afd5ba50043623d2b43bf446d84ac7
cardaguh <cardaguh@gmail.com>
2018-06-18 16:53:28 -0500

    first commit

---
+++ app/src/main/java/com/jikansoftware/volleyejemplosencillo/MainActivity.java
@@ -0,0 +1,55 @@
+package com.jikansoftware.volleyejemplosencillo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
+    String server_url = "http://192.168.1.62/saludos.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.txtRespuestaServidor);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url,
+
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                                textView.setText(response);
+                                requestQueue.stop();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("Hubo un error...");
+                        error.printStackTrace();
+                        requestQueue.stop();
+                    }
+                });
+            }
+        });
+    }
+}
